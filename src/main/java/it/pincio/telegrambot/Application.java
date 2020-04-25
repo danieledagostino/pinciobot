628929f7b4ca97f21f45a1479271ce38571a96e5
Daniele <principelele@gmail.com>
2020-04-25 17:37:14 +0200

    first commit

---
+++ src/main/java/it/pincio/telegrambot/Application.java
@@ -0,0 +1,35 @@
+
+package it.pincio.telegrambot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import it.pincio.telegrambot.controller.FirstEntryPointController;
+
+@SpringBootApplication
+public class Application 
+{
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) 
+	{
+		ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new FirstEntryPointController());
+        } catch (TelegramApiException e) {
+            logger.error("Registration hook error", e);
+        }
+        
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/it/pincio/telegrambot/test/service/TestSecurity.java
@@ -0,0 +1,46 @@
+package it.pincio.telegrambot.test.service;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import it.pincio.telegrambot.Application;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Application.class)
+@SpringBootTest
+public class TestSecurity
+{
+//	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Before
+	public void setup()
+	{
+//		this.mockMvc = MockMvcBuilders
+//				.webAppContextSetup(wac)
+//				.apply(springSecurity()) //Attiva la sicurezza
+//				.build();
+
+	}
+	
+	private String ApiBaseUrl = "/api/articoli";
+	
+	@Test
+	//@WithAnonymousUser
+	public void D_testErrlistArtByCodArt() throws Exception
+	{
+//		mockMvc.perform(MockMvcRequestBuilders.get(ApiBaseUrl + "/cerca/codice/002000301")
+//				.accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isUnauthorized())
+//				.andReturn();
+	}  
+}
