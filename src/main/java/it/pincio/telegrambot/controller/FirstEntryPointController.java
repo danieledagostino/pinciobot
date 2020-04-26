22a720d74f8810383ad627dea1d54d12cd22934c
Daniele <principelele@gmail.com>
2020-04-26 19:42:39 +0200

    Searchable text implementation

--- src/main/java/it/pincio/telegrambot/controller/FirstEntryPointController.java
+++ src/main/java/it/pincio/telegrambot/controller/FirstEntryPointController.java
@@ -4,28 +4,43 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class FirstEntryPointController extends TelegramLongPollingBot {
 	
-	private static final Logger logger = LoggerFactory.getLogger(FirstEntryPointController.class);
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@Value("${PINCIO_BOT_TOKEN}")
-	private String token;
+	private String TOKEN;
+	
+	@Value("${PERSISTENT_SERVICE}")
+	private String PERSISTENT_SERVICE;
+	
+	private String GET_UPDATE_METHOD = "/bot/search/";
 
 	@Override
 	public void onUpdateReceived(Update update) {
 		if (update.hasMessage() && update.getMessage().hasText()) {
-	        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-	                .setChatId(update.getMessage().getChatId())
-	                .setText("hai inviato questo messaggio"+update.getMessage().getText());
 	        try {
+	        	
+	            String result = restTemplate.getForObject(PERSISTENT_SERVICE+GET_UPDATE_METHOD, String.class, update.getMessage().getText());
+	        	
+	            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+	            		.setChatId(update.getMessage().getChatId())
+	            		.setText(result);
+	            
 	            execute(message); // Call method to send the message
 	        } catch (TelegramApiException e) {
 	            e.printStackTrace();
@@ -41,11 +56,11 @@ public class FirstEntryPointController extends TelegramLongPollingBot {
 
 	@Override
 	public String getBotToken() {
-		return token;
+		return TOKEN;
 	}
 
 	@PostConstruct
 	public void start() {
-		logger.info("token: {}", token);
+		log.debug("token: {}", TOKEN);
 	}
 }
