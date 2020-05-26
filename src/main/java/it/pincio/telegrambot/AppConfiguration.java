c59b72a0a79860e8337211dcb0d7b5900ee2ed7f
Daniele <principelele@gmail.com>
2020-05-26 19:26:41 +0200

    fix eureka client conf

--- src/main/java/it/pincio/telegrambot/AppConfiguration.java
+++ src/main/java/it/pincio/telegrambot/AppConfiguration.java
@@ -1,11 +1,15 @@
 package it.pincio.telegrambot;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableEurekaClient
+@EnableAutoConfiguration
 public class AppConfiguration {
 
 	@Bean
