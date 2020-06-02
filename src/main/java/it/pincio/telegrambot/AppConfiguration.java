04ac2aa0ce9a8a98fbdc82f96064913a6ab2b34c
D'Agostino <daniele.dagostino@accenture.com>
2020-06-02 18:01:58 +0200

    fix eureka client conf

--- src/main/java/it/pincio/telegrambot/AppConfiguration.java
+++ src/main/java/it/pincio/telegrambot/AppConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableAutoConfiguration
 public class AppConfiguration {
 	
-	@Value("BASIC_PW")
+	@Value("PINCIO_PASSWORD")
 	private String BASIC_PW;
 
 	@Bean
