012acb4f412e64f3791b4ed13d8ecbf88ff624ec
D'Agostino <daniele.dagostino@accenture.com>
2020-06-03 16:53:28 +0200

    Logging HTTP Reqs added

--- src/main/java/it/pincio/telegrambot/AppConfiguration.java
+++ src/main/java/it/pincio/telegrambot/AppConfiguration.java
@@ -1,12 +1,18 @@
 package it.pincio.telegrambot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import it.pincio.telegrambot.component.LoggingRequestInterceptor;
 
 @Configuration
 @EnableEurekaClient
@@ -18,7 +24,13 @@ public class AppConfiguration {
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.basicAuthentication("pincio", BASIC_PW).build();
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+		interceptors.add(new LoggingRequestInterceptor());
+		
+		RestTemplate restTemplate = builder.basicAuthentication("pincio", BASIC_PW).build();
+		restTemplate.setInterceptors(interceptors);
+		
+		return restTemplate;
 	 }
 
 }
