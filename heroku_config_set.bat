a8e9eca94fdab60ce9ff630491ca124513ead6c6
Daniele D'Agostino <principelele@gmail.com>
2020-06-16 23:30:32 +0200

    fix

---
+++ heroku_config_set.bat
@@ -0,0 +1,9 @@
+heroku config:set CLOUDAMQP_APIKEY=802d8a6e-cec0-45cd-a2f6-457bcc5bb47a
+heroku config:set CLOUDAMQP_URL=amqp://ggefxdli:bW142-F9BheYeb3DAU5B1SUGZw2n7qnN@squid.rmq.cloudamqp.com/ggefxdli
+heroku config:set DOMAIN_NAME=pincio-telegram-bot.herokuapp.com
+heroku config:set ELASTIC_SERVICE=https://pincio-telegram-elastic.herokuapp.com
+heroku config:set EUREKA_URL=https://user:83e7f164-18af-446a-92f8-06ef9b185bb9@telegrambot-eureka.herokuapp.com
+heroku config:set EUREKA_USER_PASSWORD=83e7f164-18af-446a-92f8-06ef9b185bb9
+heroku config:set PERSISTENT_SERVICE=https://pincio-telegram-persistence.herokuapp.com
+heroku config:set PINCIO_BOT_TOKEN=1158157085:AAH2vDu2HYu1qzQ9ScCgABNZ4h9unRF3wMY
+heroku config:set PINCIO_PASSWORD=83e7f164-18af-446a-92f8-06ef9b185bb9
