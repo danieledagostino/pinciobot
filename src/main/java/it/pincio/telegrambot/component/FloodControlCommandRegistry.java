c2b0889f6692ba04bb947921a89f8bcd30ae8b91
Daniele D'Agostino <principelele@gmail.com>
2020-06-28 11:27:24 +0200

    flood manager. New instance of CommandRegistry

---
+++ src/main/java/it/pincio/telegrambot/component/FloodControlCommandRegistry.java
@@ -0,0 +1,154 @@
+package it.pincio.telegrambot.component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.regex.Pattern;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.ICommandRegistry;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+public class FloodControlCommandRegistry implements ICommandRegistry {
+	private final Map<String, IBotCommand> commandRegistryMap = new HashMap<>();
+	private final boolean allowCommandsWithUsername;
+	private final String botUsername;
+	private BiConsumer<AbsSender, Message> defaultConsumer;
+	private final long TIME_TO_WAIT_FOR_PREVENTING_FLOOD = 120; //Represented with seconds
+	private Map<String, LocalDateTime> commandFoodControl;
+
+	/**
+     * Creates a Command registry
+     * @param allowCommandsWithUsername True to allow commands with username, false otherwise
+     * @param botUsername Bot username
+     */
+    public FloodControlCommandRegistry(boolean allowCommandsWithUsername, String botUsername) {
+        this.allowCommandsWithUsername = allowCommandsWithUsername;
+        this.botUsername = botUsername;
+        commandFoodControl = new HashMap<String, LocalDateTime>();
+    }
+
+	@Override
+	public void registerDefaultAction(BiConsumer<AbsSender, Message> defaultConsumer) {
+		this.defaultConsumer = defaultConsumer;
+	}
+
+	@Override
+	public final boolean register(IBotCommand botCommand) {
+		if (commandRegistryMap.containsKey(botCommand.getCommandIdentifier())) {
+			return false;
+		}
+		commandRegistryMap.put(botCommand.getCommandIdentifier(), botCommand);
+		return true;
+	}
+
+	@Override
+	public final Map<IBotCommand, Boolean> registerAll(IBotCommand... botCommands) {
+		Map<IBotCommand, Boolean> resultMap = new HashMap<>(botCommands.length);
+		for (IBotCommand botCommand : botCommands) {
+			resultMap.put(botCommand, register(botCommand));
+		}
+		return resultMap;
+	}
+
+	@Override
+	public final boolean deregister(IBotCommand botCommand) {
+		if (commandRegistryMap.containsKey(botCommand.getCommandIdentifier())) {
+			commandRegistryMap.remove(botCommand.getCommandIdentifier());
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public final Map<IBotCommand, Boolean> deregisterAll(IBotCommand... botCommands) {
+		Map<IBotCommand, Boolean> resultMap = new HashMap<>(botCommands.length);
+		for (IBotCommand botCommand : botCommands) {
+			resultMap.put(botCommand, deregister(botCommand));
+		}
+		return resultMap;
+	}
+
+	@Override
+	public final Collection<IBotCommand> getRegisteredCommands() {
+		return commandRegistryMap.values();
+	}
+
+	@Override
+	public final IBotCommand getRegisteredCommand(String commandIdentifier) {
+		return commandRegistryMap.get(commandIdentifier);
+	}
+
+	/**
+	 * Executes a command action if the command is registered.
+	 *
+	 * @note If the command is not registered and there is a default consumer, that
+	 *       action will be performed
+	 *
+	 * @param absSender absSender
+	 * @param message   input message
+	 * @return True if a command or default action is executed, false otherwise
+	 */
+	public final boolean executeCommand(AbsSender absSender, Message message) {
+		if (message.hasText()) {
+			String text = message.getText();
+			if (text.startsWith(BotCommand.COMMAND_INIT_CHARACTER)) {
+				String commandMessage = text.substring(1);
+				String[] commandSplit = commandMessage.split(BotCommand.COMMAND_PARAMETER_SEPARATOR_REGEXP);
+
+				String command = removeUsernameFromCommandIfNeeded(commandSplit[0]);
+
+				if (commandRegistryMap.containsKey(command)) {
+					if (floodManagerPermitThisCommand(command)) {
+					
+						String[] parameters = Arrays.copyOfRange(commandSplit, 1, commandSplit.length);
+						commandRegistryMap.get(command).processMessage(absSender, message, parameters);
+					}
+					return true;
+				} else if (defaultConsumer != null) {
+					defaultConsumer.accept(absSender, message);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean floodManagerPermitThisCommand(String command) {
+		LocalDateTime dateTime = null;
+		if (commandFoodControl.containsKey(command)) {
+			dateTime = commandFoodControl.get(command);
+			boolean permitted = dateTime.until(LocalDateTime.now(), ChronoUnit.SECONDS) > TIME_TO_WAIT_FOR_PREVENTING_FLOOD;
+			
+			if (permitted) {
+				commandFoodControl.put(command, LocalDateTime.now());
+			}
+			
+			return permitted;
+		} else {
+			commandFoodControl.put(command, LocalDateTime.now());
+			return true;
+		}
+		
+	}
+
+	/**
+	 * if {@link #allowCommandsWithUsername} is enabled, the username of the bot is
+	 * removed from the command
+	 * 
+	 * @param command Command to simplify
+	 * @return Simplified command
+	 */
+	private String removeUsernameFromCommandIfNeeded(String command) {
+		if (allowCommandsWithUsername) {
+			return command.replaceAll("(?i)@" + Pattern.quote(botUsername), "").trim();
+		}
+		return command;
+	}
+}
--- src/main/java/it/pincio/telegrambot/component/TelegramLongPollingCommandAndCallbackBot.java
+++ src/main/java/it/pincio/telegrambot/component/TelegramLongPollingCommandAndCallbackBot.java
@@ -1,5 +1,9 @@
 package it.pincio.telegrambot.component;
 
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
@@ -12,8 +16,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.CommandRegistry;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
@@ -36,46 +38,47 @@ import it.pincio.telegrambot.utility.TelegramKeyboard;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public abstract class TelegramLongPollingCommandAndCallbackBot extends DefaultAbsSender implements ICommandRegistry, LongPollingBot {
+public abstract class TelegramLongPollingCommandAndCallbackBot extends DefaultAbsSender
+		implements ICommandRegistry, LongPollingBot {
 
 	private final String CALLBACKDATA_ARGS_SEPARATOR = ",";
-	
+
 	@Autowired
 	protected MessageSource messageSource;
-	
+
 	@Value("${USER_BOT}")
 	protected String USER_BOT;
-	
+
 	@Override
-    public final void onUpdateReceived(Update update) {
-		Integer userId =  null;
+	public final void onUpdateReceived(Update update) {
+		Integer userId = null;
 		Chat chat = null;
 		Message message = null;
 		BotAndCallbackCommand botCommand = null;
 		Integer messageId = null;
 		boolean isValidCommand = false;
-        if (update.hasMessage()) {
-        	userId =  update.getMessage().getFrom().getId();
-    		chat = update.getMessage().getChat();
-            message = update.getMessage();
-            if (message.isCommand() && !filter(message)) {
-                if (!commandRegistry.executeCommand(this, message)) {
-                    //we have received a not registered command, handle it as invalid
-                    processInvalidCommandUpdate(update);
-                }else {
-                	isValidCommand = true;
-                	String text = message.getText();
-                	String commandMessage = text.substring(1);
-                    String[] commandSplit = commandMessage.split(BotCommand.COMMAND_PARAMETER_SEPARATOR_REGEXP);
+		if (update.hasMessage()) {
+			userId = update.getMessage().getFrom().getId();
+			chat = update.getMessage().getChat();
+			message = update.getMessage();
+			if (message.isCommand() && !filter(message)) {
+				if (!commandRegistry.executeCommand(this, message)) {
+					// we have received a not registered command, handle it as invalid
+					processInvalidCommandUpdate(update);
+				} else {
+					isValidCommand = true;
+					String text = message.getText();
+					String commandMessage = text.substring(1);
+					String[] commandSplit = commandMessage.split(BotCommand.COMMAND_PARAMETER_SEPARATOR_REGEXP);
 
-                    String command = removeUsernameFromCommandIfNeeded(commandSplit[0]);
-                	botCommand = getRegisteredCommand(command);
-                	
-                	log.info("Invoking command {}", command);
-                }
-                messageId = message.getMessageId();
-            }
-        } else if (update.hasCallbackQuery()) {
+					String command = removeUsernameFromCommandIfNeeded(commandSplit[0]);
+					botCommand = getRegisteredCommand(command);
+
+					log.info("Invoking command {}", command);
+				}
+				messageId = message.getMessageId();
+			}
+		} else if (update.hasCallbackQuery()) {
 			CallbackQuery cb = update.getCallbackQuery();
 			String[] args = cb.getData().split(CALLBACKDATA_ARGS_SEPARATOR);
 			userId = cb.getFrom().getId();
@@ -86,30 +89,29 @@ public abstract class TelegramLongPollingCommandAndCallbackBot extends DefaultAb
 				botCommand = getRegisteredCommand(args[0]);
 				sendMessage = botCommand.processCallback(cb, args[1]);
 				log.info("Callback - Invoking command {}", args[0]);
-				
+
 				execute(sendMessage);
-			}catch (NullPointerException e) {
+			} catch (NullPointerException e) {
 				log.error("Command {} not implemented yet", args[0]);
-			}catch (TelegramApiException e) {
+			} catch (TelegramApiException e) {
 				log.error("<sendMessage>");
 				log.error("username: {}", cb.getFrom().getUserName());
 				log.error(sendMessage.toString());
 				log.error("</sendMessage>");
 				log.error("TelegramApiException: Message not sent for the callback");
-			}catch (Exception e) {
+			} catch (Exception e) {
 				log.error("Generic error during send the message for the callback", e);
 			}
-			
+
 		}
-        
-        if (isValidCommand || update.hasCallbackQuery()) {
-	        if (botCommand.isPrivateAnswer() && !chat.getId().equals(new Long(userId))) {
-	        	InlineKeyboardMarkup replyMarkup = TelegramKeyboard.makeOneRowWithLink("Premi qui", "https://t.me/"+USER_BOT);
-				SendMessage privateMessage = new SendMessage().setChatId(chat.getId())
-						.setReplyMarkup(replyMarkup)
-						.setReplyToMessageId(messageId)
-						.setText(messageSource.getMessage("command.msg.touser", 
-		                		Arrays.asList(botCommand.getCommandIdentifier()).toArray(), Locale.ITALY));
+
+		if (isValidCommand || update.hasCallbackQuery()) {
+			if (botCommand.isPrivateAnswer() && !chat.getId().equals(new Long(userId))) {
+				InlineKeyboardMarkup replyMarkup = TelegramKeyboard.makeOneRowWithLink("Premi qui",
+						"https://t.me/" + USER_BOT);
+				SendMessage privateMessage = new SendMessage().setChatId(chat.getId()).setReplyMarkup(replyMarkup)
+						.setReplyToMessageId(messageId).setText(messageSource.getMessage("command.msg.touser",
+								Arrays.asList(botCommand.getCommandIdentifier()).toArray(), Locale.ITALY));
 				try {
 					execute(privateMessage);
 				} catch (TelegramApiException e) {
@@ -118,130 +120,130 @@ public abstract class TelegramLongPollingCommandAndCallbackBot extends DefaultAb
 					log.error("Generic error during send the message if the chat comes from public group", e);
 				}
 			}
-	        return;
-        }
-        
-        processNonCommandUpdate(update);
-    }
-	
+			return;
+		}
+
+		processNonCommandUpdate(update);
+	}
+
 	private String removeUsernameFromCommandIfNeeded(String command) {
 		return command.replaceAll("(?i)@" + Pattern.quote(getBotUsername()), "").trim();
-    }
-	
-	private final CommandRegistry commandRegistry;
+	}
 
-    /**
-     * Creates a TelegramLongPollingCommandBot using default options
-     * Use ICommandRegistry's methods on this bot to register commands
-     *
-     */
-    public TelegramLongPollingCommandAndCallbackBot() {
-        this(ApiContext.getInstance(DefaultBotOptions.class));
-    }
+	private final FloodControlCommandRegistry commandRegistry;
 
-    /**
-     * Creates a TelegramLongPollingCommandBot with custom options and allowing commands with
-     * usernames
-     * Use ICommandRegistry's methods on this bot to register commands
-     *
-     * @param options     Bot options
-     */
-    public TelegramLongPollingCommandAndCallbackBot(DefaultBotOptions options) {
-        this(options, true);
-    }
+	/**
+	 * Creates a TelegramLongPollingCommandBot using default options Use
+	 * ICommandRegistry's methods on this bot to register commands
+	 *
+	 */
+	public TelegramLongPollingCommandAndCallbackBot() {
+		this(ApiContext.getInstance(DefaultBotOptions.class));
+	}
 
-    /**
-     * Creates a TelegramLongPollingCommandBot
-     * Use ICommandRegistry's methods on this bot to register commands
-     *
-     * @param options                   Bot options
-     * @param allowCommandsWithUsername true to allow commands with parameters (default),
-     *                                  false otherwise
-     */
-    public TelegramLongPollingCommandAndCallbackBot(DefaultBotOptions options, boolean allowCommandsWithUsername) {
-        super(options);
-        this.commandRegistry = new CommandRegistry(allowCommandsWithUsername, this.getBotUsername());
-    }
+	/**
+	 * Creates a TelegramLongPollingCommandBot with custom options and allowing
+	 * commands with usernames Use ICommandRegistry's methods on this bot to
+	 * register commands
+	 *
+	 * @param options Bot options
+	 */
+	public TelegramLongPollingCommandAndCallbackBot(DefaultBotOptions options) {
+		this(options, true);
+	}
 
-    /**
-     * This method is called when user sends a not registered command. By default it will just call processNonCommandUpdate(),
-     * override it in your implementation if you want your bot to do other things, such as sending an error message
-     *
-     * @param update Received update from Telegram
-     */
-    protected void processInvalidCommandUpdate(Update update) {
-        processNonCommandUpdate(update);
-    }
+	/**
+	 * Creates a TelegramLongPollingCommandBot Use ICommandRegistry's methods on
+	 * this bot to register commands
+	 *
+	 * @param options                   Bot options
+	 * @param allowCommandsWithUsername true to allow commands with parameters
+	 *                                  (default), false otherwise
+	 */
+	public TelegramLongPollingCommandAndCallbackBot(DefaultBotOptions options, boolean allowCommandsWithUsername) {
+		super(options);
+		this.commandRegistry = new FloodControlCommandRegistry(allowCommandsWithUsername, this.getBotUsername());
+	}
 
+	/**
+	 * This method is called when user sends a not registered command. By default it
+	 * will just call processNonCommandUpdate(), override it in your implementation
+	 * if you want your bot to do other things, such as sending an error message
+	 *
+	 * @param update Received update from Telegram
+	 */
+	protected void processInvalidCommandUpdate(Update update) {
+		processNonCommandUpdate(update);
+	}
 
-    /**
-     * Override this function in your bot implementation to filter messages with commands
-     * <p>
-     * For example, if you want to prevent commands execution incoming from group chat:
-     * #
-     * # return !message.getChat().isGroupChat();
-     * #
-     *
-     * @param message Received message
-     * @return true if the message must be ignored by the command bot and treated as a non command message,
-     * false otherwise
-     * @note Default implementation doesn't filter anything
-     */
-    protected boolean filter(Message message) {
-        return false;
-    }
+	/**
+	 * Override this function in your bot implementation to filter messages with
+	 * commands
+	 * <p>
+	 * For example, if you want to prevent commands execution incoming from group
+	 * chat: # # return !message.getChat().isGroupChat(); #
+	 *
+	 * @param message Received message
+	 * @return true if the message must be ignored by the command bot and treated as
+	 *         a non command message, false otherwise
+	 * @note Default implementation doesn't filter anything
+	 */
+	protected boolean filter(Message message) {
+		return false;
+	}
 
-    @Override
-    public final boolean register(IBotCommand botCommand) {
-        return commandRegistry.register(botCommand);
-    }
+	@Override
+	public final boolean register(IBotCommand botCommand) {
+		return commandRegistry.register(botCommand);
+	}
 
-    @Override
-    public final Map<IBotCommand, Boolean> registerAll(IBotCommand... botCommands) {
-        return commandRegistry.registerAll(botCommands);
-    }
+	@Override
+	public final Map<IBotCommand, Boolean> registerAll(IBotCommand... botCommands) {
+		return commandRegistry.registerAll(botCommands);
+	}
 
-    @Override
-    public final boolean deregister(IBotCommand botCommand) {
-        return commandRegistry.deregister(botCommand);
-    }
+	@Override
+	public final boolean deregister(IBotCommand botCommand) {
+		return commandRegistry.deregister(botCommand);
+	}
 
-    @Override
-    public final Map<IBotCommand, Boolean> deregisterAll(IBotCommand... botCommands) {
-        return commandRegistry.deregisterAll(botCommands);
-    }
+	@Override
+	public final Map<IBotCommand, Boolean> deregisterAll(IBotCommand... botCommands) {
+		return commandRegistry.deregisterAll(botCommands);
+	}
 
-    @Override
-    public final Collection<IBotCommand> getRegisteredCommands() {
-        return commandRegistry.getRegisteredCommands();
-    }
+	@Override
+	public final Collection<IBotCommand> getRegisteredCommands() {
+		return commandRegistry.getRegisteredCommands();
+	}
 
-    @Override
-    public void registerDefaultAction(BiConsumer<AbsSender, Message> defaultConsumer) {
-        commandRegistry.registerDefaultAction(defaultConsumer);
-    }
+	@Override
+	public void registerDefaultAction(BiConsumer<AbsSender, Message> defaultConsumer) {
+		commandRegistry.registerDefaultAction(defaultConsumer);
+	}
 
-    @Override
-    public final BotAndCallbackCommand getRegisteredCommand(String commandIdentifier) {
-        return (BotAndCallbackCommand)commandRegistry.getRegisteredCommand(commandIdentifier);
-    }
+	@Override
+	public final BotAndCallbackCommand getRegisteredCommand(String commandIdentifier) {
+		return (BotAndCallbackCommand) commandRegistry.getRegisteredCommand(commandIdentifier);
+	}
 
-    /**
-     * @return Bot username
-     */
-    @Override
-    public abstract String getBotUsername();
+	/**
+	 * @return Bot username
+	 */
+	@Override
+	public abstract String getBotUsername();
 
-    /**
-     * Process all updates, that are not commands.
-     *
-     * @param update the update
-     * @warning Commands that have valid syntax but are not registered on this bot,
-     * won't be forwarded to this method <b>if a default action is present</b>.
-     */
-    public abstract void processNonCommandUpdate(Update update);
-    
-    @Override
+	/**
+	 * Process all updates, that are not commands.
+	 *
+	 * @param update the update
+	 * @warning Commands that have valid syntax but are not registered on this bot,
+	 *          won't be forwarded to this method <b>if a default action is
+	 *          present</b>.
+	 */
+	public abstract void processNonCommandUpdate(Update update);
+
+	@Override
 	public void clearWebhook() throws TelegramApiRequestException {
 		WebhookUtils.clearWebhook(this);
 	}
