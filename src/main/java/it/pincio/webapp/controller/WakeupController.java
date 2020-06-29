963998dda40eb063803d2ffd53e0bfc3f936203c
Daniele D'Agostino <principelele@gmail.com>
2020-06-29 15:54:20 +0200

    Google sheets managing

--- src/main/java/it/pincio/webapp/controller/WakeupController.java
+++ src/main/java/it/pincio/webapp/controller/WakeupController.java
@@ -1,9 +1,14 @@
 package it.pincio.webapp.controller;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.pincio.googlesheet.SheetsQuickstart;
 
 @RestController
 public class WakeupController {
@@ -11,6 +16,19 @@ public class WakeupController {
 
 	@GetMapping(value = "/wakeup")
 	public ResponseEntity<String> wakeup() {
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/sheet")
+	public ResponseEntity<String> sheet() {
+		
+		try {
+			SheetsQuickstart.main(null);
+		} catch (IOException | GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
